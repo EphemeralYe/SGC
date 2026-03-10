da69d5015519ef137ffc0fd18c5dfd99c8b5a6fc
Vysakh <101032366+EphemeralYe@users.noreply.github.com>
2026-03-10 14:03:49 +0530

    Update 04_selection_sort.c

--- DataStructure/record/04_selection_sort.c
+++ DataStructure/record/04_selection_sort.c
@@ -1,61 +1,60 @@
- 
-#include<stdio.h>  
-#include<math.h>  
-void selection(int arr[], int n)  
-{ 
- int i=0,j=0,small;  
- for (i=0;i<n -1;i++) 
- { 
-  small=i; // Minimum element in unsorted array  
- 
-  for (j=i+1;j<n;j++)  
-  { 
-   if (arr[j]<arr[small])  
-   { 
-    small=j;  
-   } 
-  } 
- 
-  int temp=arr[small];  
-  arr[small]=arr[i];  
-  arr[i]=temp;  
- } 
-} 
- 
-void  printArr(int a[], int n)  
-{ 
+#include <stdio.h>
+void selection(int arr[], int n) 
+{
+    int i = 0, j = 0, small, temp;
+    for (i = 0; i < n - 1; i++) 
+    {
+        small = i; // Assume the first element is the smallest
 
- int i=0;  
- for (i=0;i<n;i++)  
- { 
-  printf("%d \n",a[i]);  
- } 
-} 
- 
-int main()  
-{ 
- int n,i=0,num;  
- 
- printf("Enter total elements of the array : ");  
-        scanf("%d",&num);  
- 
- int a[num];  
- 
-        printf("Enter %d elements : \n",num);  
-        for (i=0;i<num;i++)  
-        { 
-                scanf("%d",&a[i]);  
-        } 
- 
- n=sizeof(a)/sizeof(a[0]);  
- 
- printf("Array Before Sorting : \n"); 
- printArr(a,n);  
- 
- selection(a,n);  
- 
- printf(" \nAfter Sorting Array Elements \n"); 
- printArr(a,n);  
- 
- return 0;  
+        for (j = i + 1; j < n; j++) 
+        {
+            if (arr[j] < arr[small]) 
+            {
+                small = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        temp = arr[small];
+        arr[small] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void printArr(int a[], int n) 
+{
+    int i = 0;
+    for (i = 0; i < n; i++) 
+    {
+        printf("%d ", a[i]); // Changed to space-separated for better readability
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    int n, i = 0, num;
+
+    printf("Enter total elements of the array : ");
+    if (scanf("%d", &num) != 1) return 1;
+
+    int a[num]; // Variable Length Array (C99)
+
+    printf("Enter %d elements : \n", num);
+    for (i = 0; i < num; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+
+    n = sizeof(a) / sizeof(a[0]);
+
+    printf("Array Before Sorting : \n");
+    printArr(a, n);
+
+    selection(a, n);
+
+    printf("\nAfter Sorting Array Elements : \n");
+    printArr(a, n);
+
+    return 0;
 }
