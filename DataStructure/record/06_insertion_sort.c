7cdc9d41225b6eab487b59f520d3570bec5d2867
Vysakh <101032366+EphemeralYe@users.noreply.github.com>
2026-03-10 14:04:20 +0530

    Update 06_insertion_sort.c

--- DataStructure/record/06_insertion_sort.c
+++ DataStructure/record/06_insertion_sort.c
@@ -1,52 +1,51 @@
- 
-#include<stdio.h>  
-void insertion_sort(int arr[],int n)  
-{ 
- int i,j,key;  
- 
- for(i=0;i<n;i++)  
- { 
-  key=arr[i];  
-  j=i-1; 
+#include <stdio.h>
 
-  while(j>=0 && arr[j]>key)  
-  { 
-   arr[j+1]=arr[j];  
-   j=j-1; 
-  } 
-  arr[j+1]=key;  
- 
- } 
- 
-} 
- 
-void printArr(int arr[],int n)  
-{ 
- for(int i=0;i<n;i++)  
- { 
-  printf("%d \n",arr[i]);  
- } 
-} 
- 
-int main()  
-{ 
- int arr[100],i,n;  
- 
- printf("Enter the total values of the array : ");  
- scanf("%d",&n);  
- 
- printf("Enter %d elements of the array : \n",n); 
- for (i=0;i<n;i++)  
- { 
-  scanf("%d",&arr[i]);  
- } 
- 
- //n=sizeof(arr)/sizeof(arr[0]);  
- 
- printf("Sorted Array : \n"); 
+void insertion_sort(int arr[], int n) 
+{
+    int i, j, key;
 
- insertion_sort(arr,n);  
- printArr(arr,n);  
- 
- return 0;  
+    for (i = 1; i < n; i++) 
+    {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1] that are greater than key
+           to one position ahead of their current position */
+        while (j >= 0 && arr[j] > key) 
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArr(int arr[], int n) 
+{
+    int i;
+    for (i = 0; i < n; i++) 
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    int arr[100], i, n;
+
+    printf("Enter the total values of the array : ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements of the array : \n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nSorted Array : \n");
+    insertion_sort(arr, n);
+    printArr(arr, n);
+
+    return 0;
 }
