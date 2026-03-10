b34a4c25f05e728f9d80be074c152314a779c93b
Vysakh <101032366+EphemeralYe@users.noreply.github.com>
2026-03-10 14:02:40 +0530

    Update 01_array_operations.c

--- DataStructure/record/01_array_operations.c
+++ DataStructure/record/01_array_operations.c
@@ -1,123 +1,87 @@
-#include<stdio.h>  
-int n, a[100];  
-int main()  
-{ 
-        void merge(int a[], int);  
-        void traverson(int a[], int);  
-        int insertion(int a[],int);  
-        void deletion(int a[],int);  
- 
-        int i,fun,op;  
- 
-        printf("Enter total elements of the array : ");  
-        scanf("%d",&n);  
-        printf("Enter %d elements : \n",n); 
-        for (i=0;i<n;i++)  
-         { 
-                scanf("%d",&a[i]);  
-        } 
- 
- do 
- { 
- 
- printf(" \nEnter the operation you want to execute : \n 1 - 
-Insertion \n 2 - Deletion \n 3 - Traverson \n 4 - Merging Two arrays \n 
-5 - Exit \n\n "); 
-        scanf("%d",&op);  
- 
+#include <stdio.h>
+void traversal(int *a, int n) {
+    printf("Array elements: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+int insertion(int *a, int n) {
+    int pos, num;
+    printf("Enter the position (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+    printf("Enter the value to be inserted: ");
+    scanf("%d", &num);
 
-  switch(op)  
-  { 
-   case 1: insertion(a,n);  
-    break; 
-   case 2: deletion(a,n);  
-    break; 
-   case 3: traverson(a,n);  
-    break; 
-   case 4: merge(a,n);  
-    break; 
-   case 5: printf("Exit Program \n"); 
-    break; 
-   default : printf("Invalid Selection \n"); 
-    break; 
-  } 
- 
- } while (op != 5);  
- 
-        return 0;  
-} 
- 
-void merge(int a[],int n)  
-{ 
- int j,n2,i;  
- 
- printf(" \nMERGING TWO ARRAYS \n"); 
- 
- 
- printf("Enter total elements of the second array : ");  
- scanf("%d",&n2);  
- printf("Enter the elements of second array : \n"); 
- for (j=n;j<n+n2;j++)  
- { 
-  scanf("%d",&a[j]);  
- } 
+    pos = pos - 1;
 
- printf("The updated array is : \n"); 
- for (i=0;i<n+n2;i++)  
- { 
-  printf("%d \n",a[i]);  
- } 
-} 
-void traverson(int a[],int n)  
-{ 
- int i; 
- printf(" \nTRAVERSON (Display Entered Array) \n"); 
- printf("Your array is : \n"); 
-        for (i=0;i<n;i++)  
-        { 
-                printf("%d \n",a[i]);  
-        } 
-} 
- 
- 
-int insertion(int a[],int n)  
-{ 
-        int p,i,num,number;  
- printf(" \nINSERTION \n"); 
-        printf("Please Enter the number to insert : ");  
-        scanf("%d",&number);  
-        printf("Enter the position to insert : ");  
-        scanf("%d",&num);  
-        p=num-1; 
-        for(i=n;i>p;i --) 
-        { 
-                a[i]=a[i -1]; 
- 
-        } 
-        a[i]=number;  
-        for (i=0;i<=n;i++)  
-        { 
+    for (int i = n; i > pos; i--) {
+        a[i] = a[i - 1];
+    }
+    a[pos] = num;
+   
+    return n + 1; 
+}
+int deletion(int *a, int n) {
+    int pos;
+    printf("Enter the position to delete (1 to %d): ", n);
+    scanf("%d", &pos);
 
-                printf("%d \n",a[i]);  
-        } 
- return 0;  
-} 
- 
-void deletion(int a[], int n)  
-{ 
- 
- int i,p,num;  
- printf(" \nDELETION \n"); 
-        printf("Element position to delete : ");  
-        scanf("%d",&num);  
-        p=num-1; 
-        for(i=p;i<n;i++)  
-        { 
-                a[i]=a[i+1];  
-        } 
-        for(i=0;i<n -1;i++) 
-        { 
-                printf("%d \n",a[i]);  
-        } 
- 
+    pos = pos - 1;
+    for (int i = pos; i < n - 1; i++) {
+        a[i] = a[i + 1];
+    }
+    
+    return n - 1;
+}
+int merge(int *a, int n) 
+{
+    int nn, b[100];
+
+    printf("How many new elements? ");
+    scanf("%d", &nn);
+
+    printf("Enter the %d elements: ", nn);
+    for(int i = 0; i < nn; i++) {
+        scanf("%d", &b[i]);
+    }
+    for(int i = 0; i < nn; i++) {
+        a[n + i] = b[i]; 
+    }
+    int total_elements = n + nn;
+    return total_elements; 
+}
+int main() {
+    int a[100], n, op;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("\n1) Insertion\n2) Deletion\n3) Traversal\n4)Merge\n");
+    printf("Choose option: ");
+    scanf("%d", &op);
+
+    switch (op) {
+        case 1:
+                n= insertion(a, n);
+                traversal(a, n);
+                break;
+        case 2:
+                n=deletion(a, n);
+                traversal(a, n);
+                break;
+	case 3:
+        	traversal(a, n);
+                break;
+	case 4:
+		n=merge(a,n);
+		traversal(a,n);
+		break;
+        default:
+                printf("Invalid option!\n");
+    }
+    return 0;
 }
