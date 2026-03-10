cb3f48ffae5e88fe46db0964f272ee7806eb792a
Vysakh <101032366+EphemeralYe@users.noreply.github.com>
2026-03-10 14:09:26 +0530

    Update 16_factorial_recursion.c

--- DataStructure/record/16_factorial_recursion.c
+++ DataStructure/record/16_factorial_recursion.c
@@ -1,24 +1,33 @@
+#include <stdio.h>
+int fact(int n) 
+{
+    if (n == 0) 
+    {
+        return 1;
+    } 
+    else 
+    {
+        // Recursive step: n * (n-1)!
+        return n * fact(n - 1);
+    }
+}
 
- 
-#include<stdio.h>  
-int fact(int n)  
-{ 
- if (n==0)  
- { 
-  return 1;  
- } 
- else 
- { 
-  return n*fact(n -1); 
- } 
-} 
- 
-int main()  
-{ 
- int n,f;  
- printf("Enter a number to find factorial : ");  
- scanf("%d",&n);  
- f=fact(n);  
- printf("Factorial = %d \n",f); 
- return 0;  
+int main() 
+{
+    int n, f;
+
+    printf("Enter a number to find factorial: ");
+    if (scanf("%d", &n) != 1) return 1;
+
+    if (n < 0) 
+    {
+        printf("Factorial of a negative number is not defined.\n");
+    } 
+    else 
+    {
+        f = fact(n);
+        printf("Factorial of %d = %d \n", n, f);
+    }
+
+    return 0;
 }
